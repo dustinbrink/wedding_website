a9fe700fb213a4c746f3922b5a9c30bbe873bf04
Dustin Brink <dustin.brink@datuhealth.com>
2015-03-15 20:37:40 -0600

    work on the style

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -4,11 +4,26 @@ $(document).ready(function() {
         $('#nav a').removeClass('selected');
         $('#content').children().addClass('hide');
         tab.addClass('selected');
-        $('#'+tab.text()).removeClass('hide');
+        var tabText = tab.text();
+        $('#'+tabText).removeClass('hide');
+        if(tabText == 'Location') {
+            resetMap();
+        }
+    };
+
+    var resetMap = function() {
+        var map = $('#map');
+        var src = map.attr('src');
+        map.attr('src', src);
     };
 
     $('#nav a').on('click', function() {
         selectTab($(this));
+    });
+
+    $('#rsvp').submit(function(e) {
+        alert( "Handler for .submit() called." );
+        e.preventDefault();
     });
 
     selectTab($('#nav a').first());
